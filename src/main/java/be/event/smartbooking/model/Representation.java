b573cc37ae9061e1232fe32d7e2f966d13440613
Losseni Dia <losdiakite@gmail.com>
2025-10-09 13:28:54 +0200

    Relation ManyToMany between User & Representation via la table r

--- src/main/java/be/event/smartbooking/model/Representation.java
+++ src/main/java/be/event/smartbooking/model/Representation.java
@@ -1,12 +1,16 @@
 package be.event.smartbooking.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -37,6 +41,37 @@ public class Representation {
     @JoinColumn(name = "location_id", nullable = true)
     private Location location;
 
+    @ManyToMany
+	@JoinTable(
+		  name = "reservations", 
+		  joinColumns = @JoinColumn(name = "representation_id"), 
+		  inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users = new ArrayList<>();
+
+    
+    //Méthodes
+    
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public Representation addUser(User user) {
+        if (!this.users.contains(user)) {
+            this.users.add(user);
+            user.addRepresentation(this);
+        }
+
+        return this;
+    }
+
+    public Representation removeUser(User user) {
+        if (this.users.contains(user)) {
+            this.users.remove(user);
+            user.getRepresentations().remove(this);
+        }
+
+        return this;
+    }
    
     
 }
--- src/main/java/be/event/smartbooking/repository/UserRepos.java
+++ src/main/java/be/event/smartbooking/repository/UserRepos.java
@@ -7,10 +7,13 @@ import org.springframework.data.repository.CrudRepository;
 import be.event.smartbooking.model.User;
 
 public interface UserRepos extends CrudRepository<User, Long> {
+    User findById(long id);
+
     User findByLogin(String login);
+    User findByEmail(String email);
 
     List<User> findByLastname(String lastname);
 
-    User findById(long id);
+   
     
 }
