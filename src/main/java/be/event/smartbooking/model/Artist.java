e1857d4c53429d368048c9077ecbc09a59a88fa6
Losseni Dia <losdiakite@gmail.com>
2025-10-09 11:31:10 +0200

    Relation ManyToMany between Artist & Type

--- src/main/java/be/event/smartbooking/model/Artist.java
+++ src/main/java/be/event/smartbooking/model/Artist.java
@@ -1,9 +1,13 @@
 package be.event.smartbooking.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -27,6 +31,35 @@ public class Artist {
     @NotBlank(message = "Le nom est obligatoire")  
     @Size(min = 2, max = 60, message = "Le nom doit contenir entre 2 et 60 caractères")
     private String lastname;
+
+    @ManyToMany(mappedBy = "artists")
+    private List<Type> types = new ArrayList<>();
+
+
+    // Méthodes
+    public List<Type> getTypes() {
+        return types;
+    }
+
+	public Artist addType(Type type) {
+		if(!this.types.contains(type)) {
+			this.types.add(type);
+			type.addArtist(this);
+		}
+		
+		return this;
+	}
+	
+	public Artist removeType(Type type) {
+		if(this.types.contains(type)) {
+			this.types.remove(type);
+			type.getArtists().remove(this);
+		}
+		
+		return this;
+	}
+
+
 
 
     @Override
--- src/main/java/be/event/smartbooking/model/Type.java
+++ src/main/java/be/event/smartbooking/model/Type.java
@@ -1,9 +1,15 @@
 package be.event.smartbooking.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +23,36 @@ public class Type {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String type;
+
+    @ManyToMany
+	@JoinTable(
+		  name = "artist_type", 
+		  joinColumns = @JoinColumn(name = "type_id"), 
+		  inverseJoinColumns = @JoinColumn(name = "artist_id"))
+    private List<Artist> artists = new ArrayList<>();
+
+
+    //Méthodes
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public Type addArtist(Artist artist) {
+        if (!this.artists.contains(artist)) {
+            this.artists.add(artist);
+            artist.addType(this);
+        }
+
+        return this;
+    }
+
+    public Type removeType(Artist artist) {
+        if (this.artists.contains(artist)) {
+            this.artists.remove(artist);
+            artist.getTypes().remove(this);
+        }
+
+        return this;
+    }
     
 }
