8308e7d0843fc0fa897b1a77741d6b5f9558b1e4
Losseni Dia <losdiakite@gmail.com>
2026-01-23 23:36:45 +0100

    création de la du service de gestion des images

--- src/main/java/be/event/smartbooking/dto/ShowCreateRequest.java
+++ src/main/java/be/event/smartbooking/dto/ShowCreateRequest.java
@@ -1,11 +1,14 @@
 package be.event.smartbooking.dto;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class ShowCreateRequest {
     private String title;
     private String description;
-    private Long locationId; // On envoie l'ID du lieu plutôt que l'objet complet
+    private Long locationId;
     private boolean bookable;
+    private List<Long> artistTypeIds; // Liste des IDs des artistes/types sélectionnés
 }
