a55acccdec0fff8aff97e617d3ae71744e174531
Krefel <elisabethbenga05@gmail.com>
2026-02-07 17:43:53 +0100

    ajout de la methode findByReservation dans RepresentationReservationRepository

--- src/main/java/be/event/smartbooking/repository/RepresentationReservationRepository.java
+++ src/main/java/be/event/smartbooking/repository/RepresentationReservationRepository.java
@@ -2,10 +2,16 @@
 package be.event.smartbooking.repository;
 
 import be.event.smartbooking.model.RepresentationReservation;
+import be.event.smartbooking.model.Reservation;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepresentationReservationRepository
         extends JpaRepository<RepresentationReservation, Long> {
+                
+        List<RepresentationReservation> findByReservation(Reservation reservation);
 }
