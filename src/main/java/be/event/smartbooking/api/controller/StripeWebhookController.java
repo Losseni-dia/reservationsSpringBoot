cd7c001a90a99d4e4497bf102d2bcfd8603fed4f
Krefel <elisabethbenga05@gmail.com>
2026-02-07 18:50:23 +0100

    mise à jour du contenu de private ReservationService reservationService et création du fichier .env pour rendre invisible les stripe keys

--- src/main/java/be/event/smartbooking/api/controller/StripeWebhookController.java
+++ src/main/java/be/event/smartbooking/api/controller/StripeWebhookController.java
@@ -1,5 +1,47 @@
 package be.event.smartbooking.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stripe.model.Event;
+import com.stripe.model.checkout.Session;
+import com.stripe.net.Webhook;
+
+import be.event.smartbooking.service.ReservationService;
+
+
+@RestController
+@RequestMapping("/api/webhooks")
 public class StripeWebhookController {
 
+    @Value("${stripe.webhook.secret}")
+    private String endpointSecret;
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @PostMapping("/stripe")
+    public ResponseEntity<String> handleStripeEvent(@RequestBody String payload, @RequestHeader("Stripe-Signature") String sigHeader) {
+        try {
+            Event event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
+
+            if ("checkout.session.completed".equals(event.getType())) {
+                Session session = (Session) event.getDataObjectDeserializer().getObject().get();
+                Long reservationId = Long.parseLong(session.getMetadata().get("reservation_id"));
+                
+                // On passe le statut à CONFIRMED
+                reservationService.confirmReservation(reservationId);
+            }
+
+            return ResponseEntity.ok("");
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("Webhook Error");
+        }
+    }
 }
