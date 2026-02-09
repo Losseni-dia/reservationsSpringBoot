ef4d15e9d35a54b56d1eef853bf706bc1ac242b1
Mariannem <marianne.anjum@gmail.com>
2026-02-09 23:55:32 +0100

    git commit -m "chore(security): add DisabledException import"

--- src/main/java/be/event/smartbooking/config/CustomUserDetailsService.java
+++ src/main/java/be/event/smartbooking/config/CustomUserDetailsService.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import org.springframework.security.authentication.DisabledException;;;
 import be.event.smartbooking.model.Role;
 import be.event.smartbooking.model.User;
 import be.event.smartbooking.repository.UserRepos;
