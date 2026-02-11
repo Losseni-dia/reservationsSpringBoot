4e15e34a60994d1cde109167a6f70f8d0027f1c9
Krefel <elisabethbenga05@gmail.com>
2026-02-11 12:36:49 +0100

    feat: ajout des méthodes de recherche pour les items de réservation

--- src/main/java/be/event/smartbooking/repository/RepresentationReservationRepository.java
+++ src/main/java/be/event/smartbooking/repository/RepresentationReservationRepository.java
@@ -7,6 +7,8 @@ import be.event.smartbooking.model.Reservation;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,4 +16,11 @@ public interface RepresentationReservationRepository
         extends JpaRepository<RepresentationReservation, Long> {
                 
         List<RepresentationReservation> findByReservation(Reservation reservation);
+
+        @Query("SELECT rr FROM RepresentationReservation rr " +
+       "JOIN FETCH rr.representation repr " +
+       "JOIN FETCH repr.show " +
+       "JOIN FETCH rr.price " +
+       "WHERE rr.reservation = :res")
+List<RepresentationReservation> findByReservationWithDetails(@Param("res") Reservation res);
 }
