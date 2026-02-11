8d01553e9b062c8bf93e9cc586d1d8a8a9538b39
Krefel <elisabethbenga05@gmail.com>
2026-02-11 12:36:30 +0100

    fix: configuration du mapping JSON sur le champ quantity via @JsonProperty

--- src/main/java/be/event/smartbooking/dto/ReservationItemRequest.java
+++ src/main/java/be/event/smartbooking/dto/ReservationItemRequest.java
@@ -1,8 +1,17 @@
 package be.event.smartbooking.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 // ReservationItemRequest.java
 public record ReservationItemRequest(
         Long representationId,
         Long priceId,
+
+        @NotNull(message = "La quantité est obligatoire")
+    @Min(1)
+    @JsonProperty("places")
         Integer quantity) {
 }
