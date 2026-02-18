61fb930cf36aed97c35ce50ac0f03c95914ff970
Krefel <elisabethbenga05@gmail.com>
2026-02-18 08:21:42 +0100

    création controller de notre Flux rss : RssController

---
+++ src/main/java/be/event/smartbooking/controller/RssController.java
@@ -0,0 +1,28 @@
+package be.event.smartbooking.controller;
+
+import be.event.smartbooking.service.ShowService;
+import be.event.smartbooking.view.RssFeedView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.View;
+
+@Controller
+public class RssController {
+
+    @Autowired
+    private ShowService showService;
+
+    @Autowired
+    private RssFeedView rssFeedView;
+
+    // Endpoint configuré sur /api/rss 
+    @GetMapping("/api/rss")
+    public View getRssFeed(Model model) {
+        // On utilise la méthode getAllBookableShows() du ShowController
+        model.addAttribute("shows", showService.getAllBookableShows());
+        
+        return rssFeedView;
+    }
+}
