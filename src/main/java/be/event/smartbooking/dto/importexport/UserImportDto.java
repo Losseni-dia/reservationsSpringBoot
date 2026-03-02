a031006a1d0d63749cf0b6f45b85d6353bd1334c
Mariannem <marianne.anjum@gmail.com>
2026-03-02 22:19:29 +0100

    create importresult dto

---
+++ src/main/java/be/event/smartbooking/dto/importexport/UserImportDto.java
@@ -0,0 +1,32 @@
+package be.event.smartbooking.dto.importexport;
+
+import be.event.smartbooking.dto.importexport.UserImportDto;
+import be.event.smartbooking.dto.importexport.ShowImportDto;
+import be.event.smartbooking.dto.importexport.ImportResult;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserImportDto {
+
+    @NotBlank(message = "Le champ 'login' est obligatoire")
+    @Size(max = 50, message = "Le login ne doit pas dépasser 50 caractères")
+    private String login;
+
+    @NotBlank(message = "Le champ 'firstname' est obligatoire")
+    private String firstname;
+
+    @NotBlank(message = "Le champ 'lastname' est obligatoire")
+    private String lastname;
+
+    @NotBlank(message = "Le champ 'email' est obligatoire")
+    @Email(message = "L'email n'est pas valide")
+    private String email;
+
+    @NotBlank(message = "Le champ 'password' est obligatoire")
+    private String password;
+
+    private String langue;
+}
